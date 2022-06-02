24e509d357f3cb4c7eb92d91daeb973cfd27df0e
SearchAndRescueAdam <80280742+SearchAndRescueAdam@users.noreply.github.com>
2022-06-02 10:52:27 -0600

    Added login req

--- index.js
+++ index.js
@@ -1,7 +1,23 @@
 import express from "express";
+import bodyParser from "body-parser";
 
 const application = express();
 
+application.use(bodyParser.json());
+
 application.get('/',(req,res)=>{res.send("Hello")});
+
+application.post('/login', (req,res)=>{
+    const userName = loginRequest.body.userName;
+    const password = loginRequest.body.password;
+
+    if(userName=="yourmom@gmail.com" && password=="P@ssw0rd"){
+        loginResponse.status(200);
+        loginResponse.send({Status:"LoggedIn"});
+    } else{
+        loginResponse.status(403);
+        loginResponse.send({Status:"FailedLoggingIn"})
+    }
+});
 
 application.listen(3000,()=>{console.log("Listening...")});
